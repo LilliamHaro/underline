4b3d8676c0a3bf3339e8067d9d528f22fb48e9e1
LilliamHaro <qualia950@gmail.com>
2018-10-24 13:55:36 -0500

    txt hv

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+  // chat-bot
   $("form button").on("click", function(event) {
     event.preventDefault();
     var texto = $("form input").val();
@@ -10,3 +11,24 @@ window.onload = function() {
     }
   });
 };
+
+
+// ---letters
+
+jQuery(document).ready(function(){
+  jQuery(".titleWrapper").addClass("ready");
+  
+jQuery(".titleWrapper h1").each(function(){
+  var fullString;
+  var characters = jQuery(this).text().split("");
+  
+  $this = jQuery(this);
+  $this.empty();
+  $.each(characters, function (i, el) {
+      if(el == " "){el = "&nbsp;"};
+  $this.append("<span>" + el + "</span");
+  });
+      
+});
+  
+});
